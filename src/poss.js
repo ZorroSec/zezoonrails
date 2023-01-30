fba1813747c13c8c1dc4898171dacf04c9d02eb4
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-01-30 19:13:38 -0300

    init

--- src/poss.js
+++ src/poss.js
@@ -1,6 +1,6 @@
 function poss(posts){
     const url = "http://servicodados.ibge.gov.br/api/v3/noticias/"
-    const methods = { method: 'GET', referrerPolicy: 'unsafe-url', mode: 'cors' }
+    const methods = { method: 'GET', referrerPolicy: 'unsafe-url', mode: 'no-cors' }
     fetch(url, methods).then((res)=>{
         res.json().then(data=>{
             for(let i=0;i<=99;i++){
